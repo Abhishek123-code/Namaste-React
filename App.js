d906929e3b124aa35c0eea7e04072cfad72f6bb0
Abhishek Ojha <aojha6159@gmail.com>
2024-08-28 17:14:41 +0530

    installed package manager ,added gitignore file

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
-/**
- *
- *
- * <div id="parent">
- *      <div  id="child1">
- *          <h1>This is h1</h1>
- *          <h2>This is h2</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>This is h1</h1>
- *          <h2>This is h2</h2>
- *      </div>
- * </div>
- *
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
