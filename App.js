fd82ff51d7b4e453a39d165346c11f995d98d11d
Abhishek Ojha <aojha6159@gmail.com>
2024-08-30 19:40:43 +0530

    added chapter_3 codes

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is h1"),
-    React.createElement("h2", {}, "This is h2"),
-  ]),
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is h1"),
-    React.createElement("h2", {}, "This is h2"),
-  ]),
-]);
+const elem = <span>React Element</span>;
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("heading"));
+const Jsxheading = (
+  <h1 className="head">
+    {elem}
+    Namaste React using jsx 🚀
+  </h1>
+);
 
-root.render(parent);
+const data=10000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {data}
+    <h1 className="heading">Namste Reace Functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
